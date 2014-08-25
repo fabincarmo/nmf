eaaced3823bf9a297cef59e185f34da060bbb6ba
fabincarmo <fabincarmo@gmail.com>
2014-08-25 19:03:12 -0300

    Update example.py

--- example.py
+++ example.py
@@ -3,12 +3,12 @@
 
 import numpy as np
 import matplotlib.pyplot as pl
-import nnsc
+import nmf
 
 def main():
     V = np.array([[1, 1, 1, 1, 1], [0, 1, 0, 1, 0], [0, 1, 0, 1, 0]])
     r = 2
-    W, H = nnsc.NNSC(V, r, lamb=0, maxit=2000)
+    W, H = nmf.NMF(V, r, lamb=0, maxit=2000)
     Ve = np.dot(W, H)
     np.set_printoptions(precision=2, suppress=True)
     print 'W x H ~ V'
